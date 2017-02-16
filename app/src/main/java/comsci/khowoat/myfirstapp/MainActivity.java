195339e1f575b5b8fe497facd486d13f151506c4
jaruwit2538 <oatza_first@hotmail.com>
2017-02-16 11:33:47 +0700

    Button Moreinfo Complete

--- app/src/main/java/comsci/khowoat/myfirstapp/MainActivity.java
+++ app/src/main/java/comsci/khowoat/myfirstapp/MainActivity.java
@@ -1,7 +1,10 @@
 package comsci.khowoat.myfirstapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }   //Main Method onCreate
+
+    public void onClickMoreinfo(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.dlt.go.th/th/dlt-knowledge/view.php?_did=111"));
+        startActivity(intent);
+    }//Main Method onClickMoreinfo
+
+}   //Main Class
